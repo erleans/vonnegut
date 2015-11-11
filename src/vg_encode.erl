93b201d179852a223d16d2745cc60bf15b398df2
Tristan Sloughter <t@crashfast.com>
2015-11-10 18:01:53 -0600

    include size in value and support keys

--- src/vg_encode.erl
+++ src/vg_encode.erl
@@ -15,8 +15,14 @@ message_set(Id, Values) ->
 %% <<Id:64, MessageSize:32, Crc:32, MagicByte:8, Attributes:8, Key/Value>>
 message_set(Id, [], Acc) ->
     {Id, Acc};
-message_set(Id, [Value | T], Acc) ->
-    CRC = erlang:crc32(Value),
-    MessageIoList = [<<CRC:32/signed, ?MAGIC:8/signed, ?ATTRIBUTES:8/signed>>, Value],
+message_set(Id, [KeyValue | T], Acc) ->
+    KV = encode_kv(KeyValue),
+    CRC = erlang:crc32(KV),
+    MessageIoList = [<<CRC:32/signed, ?MAGIC:8/signed, ?ATTRIBUTES:8/signed>>, KV],
     MessageSize = erlang:iolist_size(MessageIoList),
     message_set(Id+1, T, [Acc | [<<Id:64/signed, MessageSize:32/signed>>, MessageIoList]]).
+
+encode_kv({Key, Value}) ->
+    [<<(erlang:byte_size(Key)):32/signed>>, Key, <<(erlang:byte_size(Value)):32/signed>>, Value];
+encode_kv(Value) ->
+    <<-1:32/signed, (erlang:byte_size(Value)):32/signed, Value/binary>>.
